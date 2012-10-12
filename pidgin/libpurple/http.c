106e3212c77b85711443bc4b8df185cc15a5ee7b
Tomasz Wasilczyk <tomkiewicz@cpw.pidgin.im>
2012-10-13 01:17:01 +0200

    Basic API and testing actions for it

--- pidgin/libpurple/http.c
+++ pidgin/libpurple/http.c
@@ -26,3 +26,214 @@
 
 #include "http.h"
 
+#include "internal.h"
+#include "debug.h"
+
+struct _PurpleHttpRequest
+{
+	int ref_count;
+
+	gchar *url;
+};
+
+struct _PurpleHttpConnection
+{
+	PurpleConnection *gc;
+	PurpleHttpCallback callback;
+	gpointer user_data;
+
+	PurpleHttpResponse *response;
+};
+
+struct _PurpleHttpResponse
+{
+	int code;
+
+	gchar *data;
+	gsize data_len;
+};
+
+static PurpleHttpConnection * purple_http_connection_new(void);
+static void purple_http_connection_terminate(PurpleHttpConnection *hc);
+
+static PurpleHttpResponse * purple_http_response_new(void);
+static void purple_http_response_free(PurpleHttpResponse *response);
+
+/*** Performing HTTP requests *************************************************/
+
+PurpleHttpConnection * purple_http_get(PurpleConnection *gc, const gchar *url,
+	PurpleHttpCallback callback, gpointer user_data)
+{
+	PurpleHttpRequest *request;
+	PurpleHttpConnection *hc;
+
+	g_return_val_if_fail(url != NULL, NULL);
+
+	request = purple_http_request_new(url);
+	hc = purple_http_request(gc, request, callback, user_data);
+	purple_http_request_unref(request);
+
+	return hc;
+}
+
+static gboolean purple_http_request_dummy_timeout(gpointer user_data)
+{
+	PurpleHttpConnection *hc = user_data;
+	PurpleHttpResponse *response = hc->response;
+
+	response->code = 200;
+	response->data = g_strdup("[dummy reply]");
+	response->data_len = strlen(response->data);
+
+	purple_http_connection_terminate(hc);
+
+	return FALSE;
+}
+
+PurpleHttpConnection * purple_http_request(PurpleConnection *gc,
+	PurpleHttpRequest *request, PurpleHttpCallback callback,
+	gpointer user_data)
+{
+	PurpleHttpConnection *hc;
+
+	g_return_val_if_fail(request != NULL, NULL);
+
+	hc = purple_http_connection_new();
+	hc->gc = gc;
+	hc->callback = callback;
+	hc->user_data = user_data;
+
+	purple_debug_misc("http", "Performing new request %p for %s.\n",
+		hc, request->url);
+
+	purple_timeout_add_seconds(1, purple_http_request_dummy_timeout, hc);
+
+	return hc;
+}
+
+/*** HTTP connection API ******************************************************/
+
+static void purple_http_connection_free(PurpleHttpConnection *hc);
+
+static PurpleHttpConnection * purple_http_connection_new(void)
+{
+	PurpleHttpConnection *hc = g_new0(PurpleHttpConnection, 1);
+
+	hc->response = purple_http_response_new();
+
+	return hc;
+}
+
+static void purple_http_connection_free(PurpleHttpConnection *hc)
+{
+	purple_http_response_free(hc->response);
+	g_free(hc);
+}
+
+/* call callback and do the cleanup */
+static void purple_http_connection_terminate(PurpleHttpConnection *hc)
+{
+	g_return_if_fail(hc != NULL);
+
+	purple_debug_misc("http", "Request %p performed %s.\n", hc,
+		purple_http_response_is_successfull(hc->response) ?
+		"successfully" : "without success");
+
+	if (hc->callback)
+		hc->callback(hc, hc->response, hc->user_data);
+
+	purple_http_connection_free(hc);
+}
+
+void purple_http_conn_cancel_all(PurpleConnection *gc)
+{
+	purple_debug_warning("http", "purple_http_conn_cancel_all: To be implemented\n");
+}
+
+/*** Request API **************************************************************/
+
+static void purple_http_request_free(PurpleHttpRequest *request);
+
+PurpleHttpRequest * purple_http_request_new(const gchar *url)
+{
+	PurpleHttpRequest *request;
+
+	g_return_val_if_fail(url != NULL, NULL);
+
+	request = g_new0(PurpleHttpRequest, 1);
+
+	request->ref_count = 1;
+	request->url = g_strdup(url);
+
+	return request;
+}
+
+static void purple_http_request_free(PurpleHttpRequest *request)
+{
+	g_free(request->url);
+	g_free(request);
+}
+
+void purple_http_request_ref(PurpleHttpRequest *request)
+{
+	g_return_if_fail(request != NULL);
+
+	request->ref_count++;
+}
+
+PurpleHttpRequest * purple_http_request_unref(PurpleHttpRequest *request)
+{
+	if (request == NULL)
+		return NULL;
+
+	g_return_val_if_fail(request->ref_count > 0, NULL);
+
+	request->ref_count--;
+	if (request->ref_count > 0)
+		return request;
+
+	purple_http_request_free(request);
+	return NULL;
+}
+
+/*** HTTP response API ********************************************************/
+
+static PurpleHttpResponse * purple_http_response_new(void)
+{
+	PurpleHttpResponse *response = g_new0(PurpleHttpResponse, 1);
+
+	return response;
+}
+
+static void purple_http_response_free(PurpleHttpResponse *response)
+{
+	g_free(response);
+}
+
+gboolean purple_http_response_is_successfull(PurpleHttpResponse *response)
+{
+	g_return_val_if_fail(response != NULL, FALSE);
+
+	return response->code == 200; /* just temporary */
+}
+
+int purple_http_response_get_code(PurpleHttpResponse *response)
+{
+	g_return_val_if_fail(response != NULL, 0);
+
+	return response->code;
+}
+
+gsize purple_http_response_get_data_len(PurpleHttpResponse *response)
+{
+	g_return_val_if_fail(response != NULL, 0);
+
+	return response->data_len;
+}
+
+const gchar * purple_http_response_get_data(PurpleHttpResponse *response)
+{
+	g_return_val_if_fail(response != NULL, NULL);
+
+	return response->data;
+}
