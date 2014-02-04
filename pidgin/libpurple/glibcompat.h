ddfaede0ad2a881063ee1838f96baf3e511cbb13
Ankit Vani <a@nevitus.org>
2014-02-05 03:45:23 +0530

    Fix some more gtk-doc warnings till dnssrv
    --HG--
    branch : gtkdoc-conversion

--- pidgin/libpurple/glibcompat.h
+++ pidgin/libpurple/glibcompat.h
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1301 USA
  */
-/**
+/*
  * SECTION:glibcompat
  * @section_id: libpurple-glibcompat
  * @short_description: <filename>glibcompat.h</filename>
