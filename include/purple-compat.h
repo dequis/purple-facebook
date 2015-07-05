ad6ee67dceee00817f7e4f1a1f3f442d61e47ee2
jgeboski <jgeboski@gmail.com>
2015-07-05 17:10:07 -0400

    configure: updated to 2a5decfdeb7b

--- include/purple-compat.h
+++ include/purple-compat.h
@@ -32,29 +32,38 @@
 #define PurpleProtocol                 void
 #define PurpleRequestCommonParameters  void
 
-#define PURPLE_CONNECTION_CONNECTED  PURPLE_CONNECTED
-#define PURPLE_IM_NOT_TYPING         PURPLE_NOT_TYPING
-#define PURPLE_IM_TYPING             PURPLE_TYPING
+#define PURPLE_CMD_FLAG_PROTOCOL_ONLY  PURPLE_CMD_FLAG_PRPL_ONLY
+#define PURPLE_CMD_P_PROTOCOL          PURPLE_CMD_P_PRPL
+#define PURPLE_CONNECTION_CONNECTED    PURPLE_CONNECTED
+#define PURPLE_IM_NOT_TYPING           PURPLE_NOT_TYPING
+#define PURPLE_IM_TYPING               PURPLE_TYPING
 
-#define PURPLE_CONVERSATION      purple_conv_chat_get_conversation
-#define PURPLE_IS_BUDDY(b)       ((b) != NULL)
-#define PURPLE_IS_CONNECTION(c)  ((c) != NULL)
+#define PURPLE_CHAT_CONVERSATION        purple_conversation_get_chat_data
+#define PURPLE_CONVERSATION             purple_conv_chat_get_conversation
+#define PURPLE_IS_ACCOUNT(a)            ((a) != NULL)
+#define PURPLE_IS_BUDDY(b)              ((b) != NULL)
+#define PURPLE_IS_CHAT_CONVERSATION(c)  ((c) != NULL)
+#define PURPLE_IS_CONNECTION(c)         ((c) != NULL)
 
 #define purple_account_get_private_alias        purple_account_get_alias
 #define purple_account_set_private_alias        purple_account_set_alias
 #define purple_blist_get_default_group()        NULL
 #define purple_blist_find_buddies               purple_find_buddies
 #define purple_blist_find_buddy                 purple_find_buddy
-#define purple_connection_error                 purple_connection_error_reason
-#define purple_connection_is_disconnecting(c)   FALSE
 #define purple_chat_conversation_add_user       purple_conv_chat_add_user
 #define purple_chat_conversation_get_id         purple_conv_chat_get_id
+#define purple_chat_conversation_has_user       purple_conv_chat_find_user
+#define purple_chat_conversation_remove_user    purple_conv_chat_remove_user
 #define purple_chat_conversation_set_topic      purple_conv_chat_set_topic
+#define purple_connection_error                 purple_connection_error_reason
+#define purple_connection_is_disconnecting(c)   FALSE
+#define purple_conversation_get_connection      purple_conversation_get_gc
 #define purple_protocol_got_user_status         purple_prpl_got_user_status
 #define purple_proxy_info_get_proxy_type        purple_proxy_info_get_type
 #define purple_request_cpar_from_connection(c)  NULL
 #define purple_roomlist_get_account(l)          ((l)->account)
 #define purple_serv_got_chat_in                 serv_got_chat_in
+#define purple_serv_got_chat_left               serv_got_chat_left
 #define purple_serv_got_im                      serv_got_im
 #define purple_serv_got_typing                  serv_got_typing
 #define purple_serv_got_typing_stopped          serv_got_typing_stopped
