49136cf3055cc950903a454c9d92e57d0e76e972
James Geboski <jgeboski@gmail.com>
2015-08-15 21:21:10 -0400

    facebook: fixed the timer using seconds instead of milliseconds

--- pidgin/libpurple/protocols/facebook/data.c
+++ pidgin/libpurple/protocols/facebook/data.c
@@ -236,7 +236,7 @@ fb_data_add_timeout(FbData *fata, const gchar *name, guint interval,
 	fb_data_clear_timeout(fata, name, TRUE);
 
 	key = g_strdup(name);
-	id = purple_timeout_add_seconds(interval, func, data);
+	id = purple_timeout_add(interval, func, data);
 	g_hash_table_replace(priv->evs, key, GUINT_TO_POINTER(id));
 }
 
