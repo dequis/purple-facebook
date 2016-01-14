242ab21e16b7784575665cef5be046c5c7b27572
James Geboski <jgeboski@gmail.com>
2016-01-14 15:36:10 -0500

    facebook: add the local user to the group chat user list

--- pidgin/libpurple/protocols/facebook/facebook.c
+++ pidgin/libpurple/protocols/facebook/facebook.c
@@ -589,6 +589,7 @@ fb_cb_api_presences(FbApi *api, GSList *press, gpointer data)
 static void
 fb_cb_api_thread(FbApi *api, FbApiThread *thrd, gpointer data)
 {
+	const gchar *name;
 	FbApiUser *user;
 	FbData *fata = data;
 	gboolean active;
@@ -612,6 +613,8 @@ fb_cb_api_thread(FbApi *api, FbApiThread *thrd, gpointer data)
 		chat = purple_serv_got_joined_chat(gc, id, tid);
 	}
 
+	name = purple_account_get_username(acct);
+	purple_chat_conversation_add_user(chat, name, NULL, 0, FALSE);
 	purple_chat_conversation_set_topic(chat, NULL, thrd->topic);
 
 	for (l = thrd->users; l != NULL; l = l->next) {
