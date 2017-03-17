402eebe93c0aeec31e9116c2513fac22f5104b8a
Yaisel Hurtado González <yaiselhg@uci.cu>
2017-03-17 11:19:22 -0400

    Added update pidgin sources support from GitHub (https://github.com/tieto/pidgin)

--- update.sh
+++ update.sh
@@ -2,10 +2,17 @@
 
 URL="https://bitbucket.org/pidgin/main"
 HASHG=$(_TMP_=$(type hg 2>&1); echo $?)
+CMD=hg
 
 if test "$HASHG" != "0"; then
     echo "hg (mercurial) not found in PATH" >&2
-    exit $HASHG
+    HASGIT=$(_TMP_=$(type git 2>&1); echo $?)
+    if test "HASGIT" != "0"; then
+        echo "git not found in PATH" >&2
+    else
+        URL="https://github.com/tieto/pidgin.git"
+        CMD=git
+    fi
 fi
 
 test -z "$srcdir" && srcdir=$(dirname "$0")
@@ -15,13 +22,20 @@ test -z "$pidgindir" && pidgindir=.pidgin
 cd "$srcdir"
 REVISION=$(cat VERSION)
 
-if ! test -d "$pidgindir/.hg"; then
+if ! test -d "$pidgindir/.$CMD"; then
     rm -rf "$pidgindir"
-    hg clone "$URL" "$pidgindir"
+    $CMD clone "$URL" "$pidgindir"
 fi
 
-hg -R "$pidgindir" -v pull
-hg -R "$pidgindir" -v update -C "$REVISION"
+if [[ "$CMD" == "hg" ]]; then
+    hg -R "$pidgindir" -v pull
+    hg -R "$pidgindir" -v update -C "$REVISION"
+elif [[ "$CMD" == "git" ]]; then
+    olddir=$(pwd)
+    cd $pidgindir
+    git pull --force
+    cd $olddir
+fi
 rm -rf pidgin
 
 for FILE in $(cat MANIFEST_PIDGIN); do
